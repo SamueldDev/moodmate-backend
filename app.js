aff6ece15bdd25d44a4ca7e8ed61354c5172f046
SamueldDev <youngsammy2018@gmail.com>
2025-09-20 19:42:25 +0100

    .

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express()
 
 setupSwagger(app) 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 app.use(cors());
  
@@ -56,7 +56,7 @@ app.use("/api/suggestions", suggestionRoute)
 
 startSever()       
 
-export default app  
+ 
 
 
  
